c0b5e434bb644fb02aeaf042e73156d9ea8d0c69
eycho <chw3872@gmail.com>
2024-11-03 23:58:19 +0900

    (은유) 페이지 레이아웃 구현 완료

--- app.py
+++ app.py
@@ -10,6 +10,26 @@ def home():
 def chat():
     return render_template('Chat.html', data='chat')
 
+@app.route('/pay')
+def pay():
+    return render_template('Payment.html', data='pay')
+
+@app.route('/confirm')
+def confirm() : 
+    return render_template('Confirm.html', data='confirm')
+
+@app.route('/finger')
+def finger() : 
+    return render_template('Finger.html', data='finger')
+
+@app.route('/print')
+def print() : 
+    return render_template('Print.html', data='print')
+
+@app.route('/done')
+def done() : 
+    return render_template('PrintDone.html', data='done')
+
 if __name__ == '__main__':  
     # app.run(debug=True) # 포트 5000 이미 사용중이면 포트에 5001 넣어주자.
     app.run('0.0.0.0', port=5001, debug=True)
